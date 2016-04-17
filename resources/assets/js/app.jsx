1f66a1b84ca2b9aaf42473513bb54a211ff29da8
Matthew Bucci <buccim2@gene.com>
2016-04-17 14:40:32 -0700

    get basic react example working

---
+++ resources/assets/js/app.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import HelloWorld from './components/helloworld.jsx';
+
+ReactDOM.render(
+    <HelloWorld phrase="ES6"/>,
+    document.body
+);
